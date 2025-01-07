3c0aac0dcd1b252da79d942ffa4e886816538796
raulodev <raulodev@gmail.com>
2025-01-07 14:59:31 -0500

    update test

--- tests/stores.py
+++ tests/stores.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 from src.task_queue_checker.storage import (
     PersistQueuePostgres,
+    PersistQueueMySql,
     PersistQueueRedis,
     PersistQueueSQLite,
 )
@@ -10,6 +11,7 @@ from src.task_queue_checker.storage import (
 load_dotenv()
 
 POSTGRES_DATABASE_URL = os.getenv("POSTGRES_DATABASE_URL")
+MYSQL_DATABASE_URL = os.getenv("MYSQL_DATABASE_URL")
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
 
@@ -18,3 +20,5 @@ storage_sqlite = PersistQueueSQLite()
 storage_redis = PersistQueueRedis(host=REDIS_HOST)
 
 storage_postgres = PersistQueuePostgres(database_url=POSTGRES_DATABASE_URL)
+
+storage_mysql = PersistQueueMySql(database_url=MYSQL_DATABASE_URL)
